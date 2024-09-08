ac5cef4485fdac11bb7a4c468368f51b603d6fb2
Katrina Knight <kat.knight@newgaea.net>
2024-09-08 23:49:49 +0100

    feat: initial Commit

---
+++ common/src/main/java/dev/katcodes/blockseekers/Constants.java
@@ -0,0 +1,11 @@
+package dev.katcodes.blockseekers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "blockseekers";
+	public static final String MOD_NAME = "Block Seekers";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
